bfffc7442a7a5fff179d6df38cfa778e62a0a3f0
Boyang Niu <boyang.niu@gmail.com>
2022-02-03 12:30:27 -0800

    CreateBounty

--- server/graphql/bounty.ts
+++ server/graphql/bounty.ts
@@ -1,6 +1,9 @@
 import { GraphQLNonNull, GraphQLObjectType, GraphQLString } from "graphql";
-import { GraphQLJSONObject } from "graphql-type-json";
+import GraphQLJSON, { GraphQLJSONObject } from "graphql-type-json";
+import Board from "../models/Board.model";
 import Bounty from "../models/Bounty.model";
+import Profile from "../models/Profile.model";
+import User from "../models/User.model";
 
 const BountyType = new GraphQLObjectType({
   name: "Bounty",
@@ -30,6 +33,41 @@ const BountyQueries = {
   },
 };
 
-const BountyMutations = {};
+const BountyMutations = {
+  createBounty: {
+    type: BountyType,
+    args: {
+      metadata: {
+        type: new GraphQLNonNull(GraphQLJSONObject),
+      },
+      board_id: {
+        type: GraphQLString,
+      },
+      twitter_handle: {
+        type: GraphQLString,
+      },
+    },
+    resolve: async (parent, args, ctx, info) => {
+      let boardId = args.board_id;
+      if (!boardId) {
+        // create new creator user acc and board
+        const user = await User.create({});
+        await Profile.create({
+          twitter_handle: args.twitter_handle,
+          user_id: user.id,
+        });
+        const board = await Board.create({
+          user_id: user.id,
+        });
+        boardId = board.id;
+      }
+      return await Bounty.create({
+        metadata: args.metadata,
+        user_id: ctx.state.user.id,
+        board_id: boardId,
+      });
+    },
+  },
+};
 
 export { BountyType, BountyQueries, BountyMutations };
