20f3e2ab4dff88ff4f5a0e93b9e544d6e6f6e793
dxu <xudavidb@gmail.com>
2022-02-04 01:48:01 -0800

    add embed generator

--- client/src/utils/youtube.ts
+++ client/src/utils/youtube.ts
@@ -1,8 +1,19 @@
+const regex = /youtube\.com.*(\?v=|\/embed\/)(.{11})/;
+
 export function getImageUrlFromYoutube(url: string) {
-  var youtube_video_id = url.match(/youtube\.com.*(\?v=|\/embed\/)(.{11})/);
+  var youtube_video_id = url?.match(regex);
 
   if (youtube_video_id && youtube_video_id.length > 0) {
     return `//img.youtube.com/vi/${youtube_video_id.pop()}/mqdefault.jpg`;
+  }
+  return null;
+}
+
+export function getEmbedUrlFromYoutube(url: string) {
+  var youtube_video_id = url?.match(regex);
+
+  if (youtube_video_id && youtube_video_id.length > 0) {
+    return `//www.youtube.com/embed/${youtube_video_id.pop()}`;
   }
   return null;
 }
