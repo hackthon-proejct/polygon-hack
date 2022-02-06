f3be0d58e37572eff24a0615a35e2de1a2d59810
dxu <xudavidb@gmail.com>
2022-02-05 21:34:27 -0800

    remove footer

--- client/pages/[twitter_handle]/index.tsx
+++ client/pages/[twitter_handle]/index.tsx
@@ -34,18 +34,6 @@ const BoardPage: NextPageWithLayout = () => {
           <>No board was specified.</>
         )}
       </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{" "}
-          <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
-        </a>
-      </footer>
     </div>
   );
 };
