bb67c877b08b56e5fe43615ad1868915485bd5df
Boyang Niu <boyang.niu@gmail.com>
2022-02-06 13:57:48 -0800

    Whoops

--- client/src/graphql/GraphQLClient.ts
+++ client/src/graphql/GraphQLClient.ts
@@ -1,7 +1,14 @@
-import { ApolloClient, InMemoryCache, from, split } from "@apollo/client";
+import {
+  ApolloClient,
+  InMemoryCache,
+  from,
+  split,
+  HttpLink,
+} from "@apollo/client";
 import { onError } from "@apollo/client/link/error";
 import { setContext } from "@apollo/client/link/context";
 import { createUploadLink } from "apollo-upload-client";
+import { WebSocketLink } from "@apollo/client/link/ws";
 import { getMainDefinition } from "@apollo/client/utilities";
 import { getLocalStorageKey } from "../utils/api_client";
 import { IS_SERVER } from "@utils/constants";
@@ -40,13 +47,31 @@ const authLink = setContext((_, { headers }) => {
   };
 });
 
-const splitLink = split(({ query }) => {
-  const definition = getMainDefinition(query);
-  return (
-    definition.kind === "OperationDefinition" &&
-    definition.operation === "subscription"
-  );
-}, from([errorLink, authLink, uploadLink]));
+let link = IS_SERVER
+  ? new HttpLink({
+      uri: `${process.env.NEXT_PUBLIC_REACT_APP_WS_PROTOCOL}${process.env.NEXT_PUBLIC_REACT_APP_SERV_HOSTNAME}/graphql`,
+    })
+  : new WebSocketLink({
+      uri: `${process.env.NEXT_PUBLIC_REACT_APP_WS_PROTOCOL}${process.env.NEXT_PUBLIC_REACT_APP_SERV_HOSTNAME}/graphql`,
+      options: {
+        reconnect: true,
+        connectionParams: {
+          Authorization: localStorage.getItem(getLocalStorageKey()),
+        },
+      },
+    });
+
+const splitLink = split(
+  ({ query }) => {
+    const definition = getMainDefinition(query);
+    return (
+      definition.kind === "OperationDefinition" &&
+      definition.operation === "subscription"
+    );
+  },
+  link,
+  from([errorLink, authLink, uploadLink])
+);
 
 let client = new ApolloClient({
   link: splitLink,
