f50332c1c60b2cdfce5a96c187606c98759a578b
Boyang Niu <boyang.niu@gmail.com>
2022-02-07 23:06:56 -0800

    New bounty mint

--- server/utils/smart_contracts/mint.ts
+++ server/utils/smart_contracts/mint.ts
@@ -1,7 +1,7 @@
 import { web3 } from "./web3";
 import configJSON from "../../../web3/build/polygon-contracts/MintSubmission.json";
 
-export const address = "0x0CA98CC8b391C3B32D2EeC7a7ae06929091a1016";
+export const address = "0x13D2265C1A88161e55B239c4079d88a8E9F108ce";
 
 const abi = configJSON.abi;
 
